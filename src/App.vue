3fb8b1248459236735d9f77b96fddf97d6a4a6e9
Francis Nduamaka <francis.nduamaka@gmail.com>
2019-11-28 18:38:40 +0100

    install app dependencies and create the base components and the homepage component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheNavbar />
+    <PageHome />
+    <TheFooter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import  PageHome from '@/pages/PageHome'
+import TheNavbar from '@/components/shared/TheNavbar'
+import TheFooter from '@/components/shared/TheFooter'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    PageHome,
+    TheNavbar,
+    TheFooter
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import 'assets/css/spacing.css';
+@import '~bulma/bulma.sass';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.bold {
+  font-weight: bold;
+}
+.cover {
+  background-size: cover;
+  /*background-position: center;*/
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+.hero {
+  position: relative;
+}
+.hero-body {
+  padding: 3rem 1.5rem;
+}
+.hero-bg {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('https://images.unsplash.com/photo-1524601500432-1e1a4c71d692?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
 }
 </style>
---
+++ src/components/shared/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Chillax</strong> by <a href="#">Bastieno</a>. Made with <span style="color: #e25555;">&hearts;</span> in Lagos
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+</style>
