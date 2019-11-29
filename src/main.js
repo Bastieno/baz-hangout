48f35f4084e8d19bbc50abf44afc5e12ab0bb466
Francis Nduamaka <francis.nduamaka@gmail.com>
2019-11-29 04:28:00 +0100

    create the CategoryItem and the MeetupItem Component, fetch the categories and the meetups data from our db using axios

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import moment from 'moment'
 import App from './App.vue'
 import AppDropdown from './components/shared/AppDropdown'
 import AppHero from './components/shared/AppHero'
@@ -7,6 +8,10 @@ Vue.config.productionTip = false
 
 Vue.component('AppHero', AppHero)
 Vue.component('AppDropdown', AppDropdown)
+
+Vue.filter('capitalize', (word = '') => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+
+Vue.filter('formatDate', (value = '', formatType = 'LL') => moment(value).format(formatType))
 
 new Vue({
   render: h => h(App),
